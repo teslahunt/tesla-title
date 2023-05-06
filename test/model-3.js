11b867fbe733c5eacb664263c479962e9650fed6
Kiko Beats <josefrancisco.verdu@gmail.com>
2023-05-06 20:12:42 +0000

    chore: add MT327

--- test/model-3.js
+++ test/model-3.js
@@ -23,6 +23,7 @@ test('Model 3 Standard Range Plus', t => {
   t.is(title({ optionCodes: ['MDL3', 'MT337'] }), 'Model 3 Standard Range Plus')
   t.is(title({ optionCodes: ['MDL3', 'MT301'] }), 'Model 3 Standard Range Plus')
   t.is(title({ optionCodes: ['MDL3', 'MT322'] }), 'Model 3 Standard Range Plus')
+  t.is(title({ optionCodes: ['MDL3', 'MT327'] }), 'Model 3 Standard Range Plus')
 })
 
 test('Model 3 Long Range RWD', t => {
