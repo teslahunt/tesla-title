6856674d561d49b504ec3af81a9f62ad3538a8c7
Kiko Beats <josefrancisco.verdu@gmail.com>
2023-10-18 20:56:15 +0200

    feat: remove repetitive words (#10)

--- index.js
+++ index.js
@@ -1,116 +1,132 @@
 'use strict'
 
-const MODEL_S = Object.entries({
-  'Model S 60D': [['BT60', 'DV4W']],
-  'Model S 60': [['BT60', 'DV2W']],
-  'Model S 70': [['BT70', 'DV2W']],
-  'Model S 70D': [['BT70', 'DV4W']],
-  'Model S 75': [
-    ['BTX5', 'DV2W'],
-    ['BTX7', 'DV2W'],
-    ['BTX8', 'DV2W']
+const MODEL_S = [
+  ['60D', [['BT60', 'DV4W']]],
+  ['60', [['BT60', 'DV2W']]],
+  ['70', [['BT70', 'DV2W']]],
+  ['70D', [['BT70', 'DV4W']]],
+  [
+    '75',
+    [
+      ['BTX5', 'DV2W'],
+      ['BTX7', 'DV2W'],
+      ['BTX8', 'DV2W']
+    ]
   ],
-  'Model S 75D': [
-    ['BTX5', 'DV4W'],
-    ['BTX7', 'DV4W'],
-    ['BTX8', 'DV4W']
+  [
+    '75D',
+    [
+      ['BTX5', 'DV4W'],
+      ['BTX7', 'DV4W'],
+      ['BTX8', 'DV4W']
+    ]
   ],
-  'Model S P85+': [['PX01']],
-  'Model S P85': [
-    ['BT85', 'X024', 'DV2W'],
-    ['PBT85', 'X024', 'DV2W']
+  ['P85+', [['PX01']]],
+  [
+    'P85',
+    [
+      ['BT85', 'X024', 'DV2W'],
+      ['PBT85', 'X024', 'DV2W']
+    ]
   ],
-  'Model S 85': [['BT85', 'DV2W']],
-  'Model S 85D': [['BT85', 'DV4W']],
-  'Model S P85D': [['P85D'], ['PBT85', 'X024', 'DV4W']],
-  'Model S P90D': [
-    ['X024', 'BTX4', 'DV4W'],
-    ['X024', 'PX4D', 'DV4W']
+  ['85', [['BT85', 'DV2W']]],
+  ['85D', [['BT85', 'DV4W']]],
+  ['P85D', [['P85D'], ['PBT85', 'X024', 'DV4W']]],
+  [
+    'P90D',
+    [
+      ['X024', 'BTX4', 'DV4W'],
+      ['X024', 'PX4D', 'DV4W']
+    ]
   ],
-  'Model S 90': [['BTX4', 'DV2W']],
-  'Model S 90D': [['BTX4', 'DV4W']],
-  'Model S P100D': [['BTX6', 'X024', 'DV4W']],
-  'Model S 100D': [['BTX6', 'DV4W']],
-  'Model S Standard Range': [['MTS01'], ['MTS17']],
-  'Model S Long Range': [['MTS03'], ['MTS05'], ['MTS10']],
-  'Model S Performance': [['MTS04'], ['MTS06'], ['MTS08']],
-  'Model S Long Range Plus': [['MTS07']],
-  'Model S': [['MTS13']],
-  'Model S Plaid': [['MTS11'], ['MTS12'], ['MTS14']],
-  'Model S Plaid+': [['MTS09']]
-})
+  ['90', [['BTX4', 'DV2W']]],
+  ['90D', [['BTX4', 'DV4W']]],
+  ['P100D', [['BTX6', 'X024', 'DV4W']]],
+  ['100D', [['BTX6', 'DV4W']]],
+  ['Standard Range', [['MTS01'], ['MTS17']]],
+  ['Long Range', [['MTS03'], ['MTS05'], ['MTS10']]],
+  ['Performance', [['MTS04'], ['MTS06'], ['MTS08']]],
+  ['Long Range Plus', [['MTS07']]],
+  ['', [['MTS13']]],
+  ['Plaid', [['MTS11'], ['MTS12'], ['MTS14']]],
+  ['Plaid+', [['MTS09']]]
+]
 
-const MODEL_X = Object.entries({
-  'Model X 75D': [
-    ['BTX5', 'DV4W'],
-    ['BTX7', 'DV4W'],
-    ['BTX8', 'DV4W']
+const MODEL_X = [
+  [
+    '75D',
+    [
+      ['BTX5', 'DV4W'],
+      ['BTX7', 'DV4W'],
+      ['BTX8', 'DV4W']
+    ]
   ],
-  'Model X P90D': [
-    ['X024', 'BTX4', 'DV4W'],
-    ['X024', 'PX4D', 'DV4W']
+  [
+    'P90D',
+    [
+      ['X024', 'BTX4', 'DV4W'],
+      ['X024', 'PX4D', 'DV4W']
+    ]
   ],
-  'Model X 90D': [['BTX4', 'DV4W']],
-  'Model X P100D': [['BTX6', 'X024', 'DV4W']],
-  'Model X 100D': [['BTX6', 'DV4W']],
-  'Model X Standard Range': [['MTX01'], ['MTX17']],
-  'Model X Long Range': [['MTX03'], ['MTX10']],
-  'Model X Long Range Plus': [['MTX05'], ['MTX07']],
-  'Model X Performance': [['MTX04'], ['MTX06'], ['MTX08']],
-  'Model X': [['MTX12'], ['MTX13'], ['MTX15']],
-  'Model X Plaid': [['MTX11'], ['MTX14'], ['MTX16']],
-  'Model X Plaid+': [['MTX09']]
-})
+  ['90D', [['BTX4', 'DV4W']]],
+  ['P100D', [['BTX6', 'X024', 'DV4W']]],
+  ['100D', [['BTX6', 'DV4W']]],
+  ['Standard Range', [['MTX01'], ['MTX17']]],
+  ['Long Range', [['MTX03'], ['MTX10']]],
+  ['Long Range Plus', [['MTX05'], ['MTX07']]],
+  ['Performance', [['MTX04'], ['MTX06'], ['MTX08']]],
+  ['', [['MTX12'], ['MTX13'], ['MTX15']]],
+  ['Plaid', [['MTX11'], ['MTX14'], ['MTX16']]],
+  ['Plaid+', [['MTX09']]]
+]
 
-const MODEL_3 = Object.entries({
-  'Model 3': [['MT322'], ['MT351']],
-  'Model 3 Standard Range': [['MT300']],
-  'Model 3 Standard Range Plus': [
-    ['MT301'],
-    ['MT308'],
-    ['MT309'],
-    ['MT314'],
-    ['MT320'],
-    ['MT327'],
-    ['MT336'],
-    ['MT337']
+const MODEL_3 = [
+  ['', [['MT322'], ['MT351']]],
+  ['Standard Range', [['MT300']]],
+  [
+    'Standard Range Plus',
+    [
+      ['MT301'],
+      ['MT308'],
+      ['MT309'],
+      ['MT314'],
+      ['MT320'],
+      ['MT327'],
+      ['MT336'],
+      ['MT337']
+    ]
   ],
-  'Model 3 Mid Range': [['MT305'], ['MT307']],
-  'Model 3 Long Range RWD': [['MT302'], ['MT341']],
-  'Model 3 Long Range AWD': [
-    ['MT303'],
-    ['MT310'],
-    ['MT315'],
-    ['MT316'],
-    ['MT321'],
-    ['MT323'],
-    ['MT324'],
-    ['MT328'],
-    ['MT352']
+  ['Mid Range', [['MT305'], ['MT307']]],
+  ['Long Range RWD', [['MT302'], ['MT341']]],
+  [
+    'Long Range AWD',
+    [
+      ['MT303'],
+      ['MT310'],
+      ['MT315'],
+      ['MT316'],
+      ['MT321'],
+      ['MT323'],
+      ['MT324'],
+      ['MT328'],
+      ['MT352']
+    ]
   ],
-  'Model 3 Performance': [
-    ['MT304'],
-    ['MT311'],
-    ['MT317'],
-    ['MT325'],
-    ['MT329'],
-    ['MT340']
+  [
+    'Performance',
+    [['MT304'], ['MT311'], ['MT317'], ['MT325'], ['MT329'], ['MT340']]
   ]
-})
+]
 
-const MODEL_Y = Object.entries({
-  'Model Y Standard Range': [['MTY01'], ['MTY13']],
-  'Model Y Long Range RWD': [['MTY02']],
-  'Model Y Long Range AWD': [
-    ['MTY03'],
-    ['MTY06'],
-    ['MTY07'],
-    ['MTY09'],
-    ['MTY11'],
-    ['MTY14']
+const MODEL_Y = [
+  ['Standard Range', [['MTY01'], ['MTY13']]],
+  ['Long Range RWD', [['MTY02']]],
+  [
+    'Long Range AWD',
+    [['MTY03'], ['MTY06'], ['MTY07'], ['MTY09'], ['MTY11'], ['MTY14']]
   ],
-  'Model Y Performance': [['MTY04'], ['MTY05'], ['MTY10'], ['MTY12']]
-})
+  ['Performance', [['MTY04'], ['MTY05'], ['MTY10'], ['MTY12']]]
+]
 
 const getCollection = optionCodes => {
   if (optionCodes.includes('MDLS')) return MODEL_S
--- test/model-s.js
+++ test/model-s.js
@@ -5,113 +5,107 @@ const test = require('ava')
 const title = require('..')
 
 test('Model S 60', t => {
-  t.is(title({ optionCodes: ['MDLS', 'BT60', 'DV2W'] }), 'Model S 60')
+  t.is(title({ optionCodes: ['MDLS', 'BT60', 'DV2W'] }), '60')
 })
 
 test('Model S 60D', t => {
-  t.is(title({ optionCodes: ['MDLS', 'BT60', 'DV4W'] }), 'Model S 60D')
+  t.is(title({ optionCodes: ['MDLS', 'BT60', 'DV4W'] }), '60D')
 })
 
 test('Model S 70', t => {
-  t.is(title({ optionCodes: ['MDLS', 'BT70', 'DV2W'] }), 'Model S 70')
+  t.is(title({ optionCodes: ['MDLS', 'BT70', 'DV2W'] }), '70')
 })
 
 test('Model S 70D', t => {
-  t.is(title({ optionCodes: ['MDLS', 'BT70', 'DV4W'] }), 'Model S 70D')
+  t.is(title({ optionCodes: ['MDLS', 'BT70', 'DV4W'] }), '70D')
 })
 
 test('Model S 75', t => {
-  t.is(title({ optionCodes: ['MDLS', 'BTX5', 'DV2W'] }), 'Model S 75')
-  t.is(title({ optionCodes: ['MDLS', 'BTX7', 'DV2W'] }), 'Model S 75')
-  t.is(title({ optionCodes: ['MDLS', 'BTX8', 'DV2W'] }), 'Model S 75')
+  t.is(title({ optionCodes: ['MDLS', 'BTX5', 'DV2W'] }), '75')
+  t.is(title({ optionCodes: ['MDLS', 'BTX7', 'DV2W'] }), '75')
+  t.is(title({ optionCodes: ['MDLS', 'BTX8', 'DV2W'] }), '75')
 })
 
 test('Model S 75D', t => {
-  t.is(title({ optionCodes: ['MDLS', 'BTX5', 'DV4W'] }), 'Model S 75D')
-  t.is(title({ optionCodes: ['MDLS', 'BTX7', 'DV4W'] }), 'Model S 75D')
-  t.is(title({ optionCodes: ['MDLS', 'BTX8', 'DV4W'] }), 'Model S 75D')
+  t.is(title({ optionCodes: ['MDLS', 'BTX5', 'DV4W'] }), '75D')
+  t.is(title({ optionCodes: ['MDLS', 'BTX7', 'DV4W'] }), '75D')
+  t.is(title({ optionCodes: ['MDLS', 'BTX8', 'DV4W'] }), '75D')
 })
 
 test('Model S 85', t => {
-  t.is(title({ optionCodes: ['MDLS', 'BT85', 'DV2W'] }), 'Model S 85')
+  t.is(title({ optionCodes: ['MDLS', 'BT85', 'DV2W'] }), '85')
 })
 
 test('Model S P85', t => {
-  t.is(title({ optionCodes: ['MDLS', 'BT85', 'X024', 'DV2W'] }), 'Model S P85')
-  t.is(title({ optionCodes: ['MDLS', 'PBT85', 'X024', 'DV2W'] }), 'Model S P85')
+  t.is(title({ optionCodes: ['MDLS', 'BT85', 'X024', 'DV2W'] }), 'P85')
+  t.is(title({ optionCodes: ['MDLS', 'PBT85', 'X024', 'DV2W'] }), 'P85')
 })
 
 test('Model S P85+', t => {
-  t.is(title({ optionCodes: ['MDLS', 'BT85', 'DV2W', 'PX01'] }), 'Model S P85+')
+  t.is(title({ optionCodes: ['MDLS', 'BT85', 'DV2W', 'PX01'] }), 'P85+')
 })
 
 test('Model S 85D', t => {
-  t.is(title({ optionCodes: ['MDLS', 'BT85', 'DV4W'] }), 'Model S 85D')
+  t.is(title({ optionCodes: ['MDLS', 'BT85', 'DV4W'] }), '85D')
 })
 
 test('Model S P85D', t => {
-  t.is(title({ optionCodes: ['MDLS', 'P85D'] }), 'Model S P85D')
-  t.is(
-    title({ optionCodes: ['MDLS', 'PBT85', 'X024', 'DV4W'] }),
-    'Model S P85D'
-  )
+  t.is(title({ optionCodes: ['MDLS', 'P85D'] }), 'P85D')
+  t.is(title({ optionCodes: ['MDLS', 'PBT85', 'X024', 'DV4W'] }), 'P85D')
 })
 
 test('Model S 90', t => {
-  t.is(title({ optionCodes: ['MDLS', 'BTX4', 'DV2W'] }), 'Model S 90')
+  t.is(title({ optionCodes: ['MDLS', 'BTX4', 'DV2W'] }), '90')
 })
 
 test('Model S 90D', t => {
-  t.is(title({ optionCodes: ['MDLS', 'BTX4', 'DV4W'] }), 'Model S 90D')
+  t.is(title({ optionCodes: ['MDLS', 'BTX4', 'DV4W'] }), '90D')
 })
 
 test('Model S P90D', t => {
-  t.is(title({ optionCodes: ['MDLS', 'X024', 'BTX4', 'DV4W'] }), 'Model S P90D')
-  t.is(title({ optionCodes: ['MDLS', 'X024', 'PX4D', 'DV4W'] }), 'Model S P90D')
+  t.is(title({ optionCodes: ['MDLS', 'X024', 'BTX4', 'DV4W'] }), 'P90D')
+  t.is(title({ optionCodes: ['MDLS', 'X024', 'PX4D', 'DV4W'] }), 'P90D')
 })
 
 test('Model S 100D', t => {
-  t.is(title({ optionCodes: ['MDLS', 'BTX6', 'DV4W'] }), 'Model S 100D')
+  t.is(title({ optionCodes: ['MDLS', 'BTX6', 'DV4W'] }), '100D')
 })
 
 test('Model S P100D', t => {
-  t.is(
-    title({ optionCodes: ['MDLS', 'X024', 'BTX6', 'DV4W'] }),
-    'Model S P100D'
-  )
+  t.is(title({ optionCodes: ['MDLS', 'X024', 'BTX6', 'DV4W'] }), 'P100D')
 })
 
 test('Model S Standard Range', t => {
-  t.is(title({ optionCodes: ['MDLS', 'MTS01'] }), 'Model S Standard Range')
-  t.is(title({ optionCodes: ['MDLS', 'MTS17'] }), 'Model S Standard Range')
+  t.is(title({ optionCodes: ['MDLS', 'MTS01'] }), 'Standard Range')
+  t.is(title({ optionCodes: ['MDLS', 'MTS17'] }), 'Standard Range')
 })
 
 test('Model S Long Range', t => {
-  t.is(title({ optionCodes: ['MDLS', 'MTS03'] }), 'Model S Long Range')
-  t.is(title({ optionCodes: ['MDLS', 'MTS05'] }), 'Model S Long Range')
-  t.is(title({ optionCodes: ['MDLS', 'MTS10'] }), 'Model S Long Range')
+  t.is(title({ optionCodes: ['MDLS', 'MTS03'] }), 'Long Range')
+  t.is(title({ optionCodes: ['MDLS', 'MTS05'] }), 'Long Range')
+  t.is(title({ optionCodes: ['MDLS', 'MTS10'] }), 'Long Range')
 })
 
 test('Model S Long Range Plus', t => {
-  t.is(title({ optionCodes: ['MDLS', 'MTS07'] }), 'Model S Long Range Plus')
+  t.is(title({ optionCodes: ['MDLS', 'MTS07'] }), 'Long Range Plus')
 })
 
 test('Model S Performance', t => {
-  t.is(title({ optionCodes: ['MDLS', 'MTS04'] }), 'Model S Performance')
-  t.is(title({ optionCodes: ['MDLS', 'MTS06'] }), 'Model S Performance')
-  t.is(title({ optionCodes: ['MDLS', 'MTS08'] }), 'Model S Performance')
+  t.is(title({ optionCodes: ['MDLS', 'MTS04'] }), 'Performance')
+  t.is(title({ optionCodes: ['MDLS', 'MTS06'] }), 'Performance')
+  t.is(title({ optionCodes: ['MDLS', 'MTS08'] }), 'Performance')
 })
 
 test('Model S', t => {
-  t.is(title({ optionCodes: ['MDLS', 'MTS13'] }), 'Model S')
+  t.is(title({ optionCodes: ['MDLS', 'MTS13'] }), '')
 })
 
 test('Model S Plaid', t => {
-  t.is(title({ optionCodes: ['MDLS', 'MTS11'] }), 'Model S Plaid')
-  t.is(title({ optionCodes: ['MDLS', 'MTS12'] }), 'Model S Plaid')
-  t.is(title({ optionCodes: ['MDLS', 'MTS14'] }), 'Model S Plaid')
+  t.is(title({ optionCodes: ['MDLS', 'MTS11'] }), 'Plaid')
+  t.is(title({ optionCodes: ['MDLS', 'MTS12'] }), 'Plaid')
+  t.is(title({ optionCodes: ['MDLS', 'MTS14'] }), 'Plaid')
 })
 
 test('Model S Plaid+', t => {
-  t.is(title({ optionCodes: ['MDLS', 'MTS09'] }), 'Model S Plaid+')
+  t.is(title({ optionCodes: ['MDLS', 'MTS09'] }), 'Plaid+')
 })
