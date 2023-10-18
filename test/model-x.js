b5799e7a5b66713e3e9298f9ef310a4e4eabe74f
Kiko Beats <josefrancisco.verdu@gmail.com>
2023-10-18 20:52:48 +0200

    feat: remove repetitive words
    the full title can still be compsed out of the library.

--- test/model-x.js
+++ test/model-x.js
@@ -5,64 +5,61 @@ const test = require('ava')
 const title = require('..')
 
 test('Model X 75D', t => {
-  t.is(title({ optionCodes: ['MDLX', 'BTX5', 'DV4W'] }), 'Model X 75D')
-  t.is(title({ optionCodes: ['MDLX', 'BTX7', 'DV4W'] }), 'Model X 75D')
-  t.is(title({ optionCodes: ['MDLX', 'BTX8', 'DV4W'] }), 'Model X 75D')
+  t.is(title({ optionCodes: ['MDLX', 'BTX5', 'DV4W'] }), '75D')
+  t.is(title({ optionCodes: ['MDLX', 'BTX7', 'DV4W'] }), '75D')
+  t.is(title({ optionCodes: ['MDLX', 'BTX8', 'DV4W'] }), '75D')
 })
 
 test('Model X 90D', t => {
-  t.is(title({ optionCodes: ['MDLX', 'BTX4', 'DV4W'] }), 'Model X 90D')
+  t.is(title({ optionCodes: ['MDLX', 'BTX4', 'DV4W'] }), '90D')
 })
 
 test('Model X P90D', t => {
-  t.is(title({ optionCodes: ['MDLX', 'X024', 'BTX4', 'DV4W'] }), 'Model X P90D')
-  t.is(title({ optionCodes: ['MDLX', 'X024', 'PX4D', 'DV4W'] }), 'Model X P90D')
+  t.is(title({ optionCodes: ['MDLX', 'X024', 'BTX4', 'DV4W'] }), 'P90D')
+  t.is(title({ optionCodes: ['MDLX', 'X024', 'PX4D', 'DV4W'] }), 'P90D')
 })
 
 test('Model X 100D', t => {
-  t.is(title({ optionCodes: ['MDLX', 'BTX6', 'DV4W'] }), 'Model X 100D')
+  t.is(title({ optionCodes: ['MDLX', 'BTX6', 'DV4W'] }), '100D')
 })
 
 test('Model X P100D', t => {
-  t.is(
-    title({ optionCodes: ['MDLX', 'X024', 'BTX6', 'DV4W'] }),
-    'Model X P100D'
-  )
+  t.is(title({ optionCodes: ['MDLX', 'X024', 'BTX6', 'DV4W'] }), 'P100D')
 })
 
 test('Model X Standard Range', t => {
-  t.is(title({ optionCodes: ['MDLX', 'MTX01'] }), 'Model X Standard Range')
-  t.is(title({ optionCodes: ['MDLX', 'MTX17'] }), 'Model X Standard Range')
+  t.is(title({ optionCodes: ['MDLX', 'MTX01'] }), 'Standard Range')
+  t.is(title({ optionCodes: ['MDLX', 'MTX17'] }), 'Standard Range')
 })
 
 test('Model X Long Range', t => {
-  t.is(title({ optionCodes: ['MDLX', 'MTX03'] }), 'Model X Long Range')
-  t.is(title({ optionCodes: ['MDLX', 'MTX10'] }), 'Model X Long Range')
+  t.is(title({ optionCodes: ['MDLX', 'MTX03'] }), 'Long Range')
+  t.is(title({ optionCodes: ['MDLX', 'MTX10'] }), 'Long Range')
 })
 
 test('Model X Long Range Plus', t => {
-  t.is(title({ optionCodes: ['MDLX', 'MTX05'] }), 'Model X Long Range Plus')
-  t.is(title({ optionCodes: ['MDLX', 'MTX07'] }), 'Model X Long Range Plus')
+  t.is(title({ optionCodes: ['MDLX', 'MTX05'] }), 'Long Range Plus')
+  t.is(title({ optionCodes: ['MDLX', 'MTX07'] }), 'Long Range Plus')
 })
 
 test('Model X Performance', t => {
-  t.is(title({ optionCodes: ['MDLX', 'MTX06'] }), 'Model X Performance')
-  t.is(title({ optionCodes: ['MDLX', 'MTX04'] }), 'Model X Performance')
-  t.is(title({ optionCodes: ['MDLX', 'MTX08'] }), 'Model X Performance')
+  t.is(title({ optionCodes: ['MDLX', 'MTX06'] }), 'Performance')
+  t.is(title({ optionCodes: ['MDLX', 'MTX04'] }), 'Performance')
+  t.is(title({ optionCodes: ['MDLX', 'MTX08'] }), 'Performance')
 })
 
 test('Model X', t => {
-  t.is(title({ optionCodes: ['MDLX', 'MTX12'] }), 'Model X')
-  t.is(title({ optionCodes: ['MDLX', 'MTX13'] }), 'Model X')
-  t.is(title({ optionCodes: ['MDLX', 'MTX15'] }), 'Model X')
+  t.is(title({ optionCodes: ['MDLX', 'MTX12'] }), '')
+  t.is(title({ optionCodes: ['MDLX', 'MTX13'] }), '')
+  t.is(title({ optionCodes: ['MDLX', 'MTX15'] }), '')
 })
 
 test('Model X Plaid', t => {
-  t.is(title({ optionCodes: ['MDLX', 'MTX11'] }), 'Model X Plaid')
-  t.is(title({ optionCodes: ['MDLX', 'MTX14'] }), 'Model X Plaid')
-  t.is(title({ optionCodes: ['MDLX', 'MTX16'] }), 'Model X Plaid')
+  t.is(title({ optionCodes: ['MDLX', 'MTX11'] }), 'Plaid')
+  t.is(title({ optionCodes: ['MDLX', 'MTX14'] }), 'Plaid')
+  t.is(title({ optionCodes: ['MDLX', 'MTX16'] }), 'Plaid')
 })
 
 test('Model X Plaid+', t => {
-  t.is(title({ optionCodes: ['MDLX', 'MTX09'] }), 'Model X Plaid+')
+  t.is(title({ optionCodes: ['MDLX', 'MTX09'] }), 'Plaid+')
 })
--- test/model-y.js
+++ test/model-y.js
@@ -5,26 +5,26 @@ const test = require('ava')
 const title = require('..')
 
 test('Model Y Standard Range', t => {
-  t.is(title({ optionCodes: ['MDLY', 'MTY01'] }), 'Model Y Standard Range')
-  t.is(title({ optionCodes: ['MDLY', 'MTY13'] }), 'Model Y Standard Range')
+  t.is(title({ optionCodes: ['MDLY', 'MTY01'] }), 'Standard Range')
+  t.is(title({ optionCodes: ['MDLY', 'MTY13'] }), 'Standard Range')
 })
 
 test('Model Y Long Range RWD', t => {
-  t.is(title({ optionCodes: ['MDLY', 'MTY02'] }), 'Model Y Long Range RWD')
+  t.is(title({ optionCodes: ['MDLY', 'MTY02'] }), 'Long Range RWD')
 })
 
 test('Model Y Long Range AWD', t => {
-  t.is(title({ optionCodes: ['MDLY', 'MTY03'] }), 'Model Y Long Range AWD')
-  t.is(title({ optionCodes: ['MDLY', 'MTY06'] }), 'Model Y Long Range AWD')
-  t.is(title({ optionCodes: ['MDLY', 'MTY07'] }), 'Model Y Long Range AWD')
-  t.is(title({ optionCodes: ['MDLY', 'MTY09'] }), 'Model Y Long Range AWD')
-  t.is(title({ optionCodes: ['MDLY', 'MTY11'] }), 'Model Y Long Range AWD')
-  t.is(title({ optionCodes: ['MDLY', 'MTY14'] }), 'Model Y Long Range AWD')
+  t.is(title({ optionCodes: ['MDLY', 'MTY03'] }), 'Long Range AWD')
+  t.is(title({ optionCodes: ['MDLY', 'MTY06'] }), 'Long Range AWD')
+  t.is(title({ optionCodes: ['MDLY', 'MTY07'] }), 'Long Range AWD')
+  t.is(title({ optionCodes: ['MDLY', 'MTY09'] }), 'Long Range AWD')
+  t.is(title({ optionCodes: ['MDLY', 'MTY11'] }), 'Long Range AWD')
+  t.is(title({ optionCodes: ['MDLY', 'MTY14'] }), 'Long Range AWD')
 })
 
 test('Model Y Performance', t => {
-  t.is(title({ optionCodes: ['MDLY', 'MTY04'] }), 'Model Y Performance')
-  t.is(title({ optionCodes: ['MDLY', 'MTY05'] }), 'Model Y Performance')
-  t.is(title({ optionCodes: ['MDLY', 'MTY10'] }), 'Model Y Performance')
-  t.is(title({ optionCodes: ['MDLY', 'MTY12'] }), 'Model Y Performance')
+  t.is(title({ optionCodes: ['MDLY', 'MTY04'] }), 'Performance')
+  t.is(title({ optionCodes: ['MDLY', 'MTY05'] }), 'Performance')
+  t.is(title({ optionCodes: ['MDLY', 'MTY10'] }), 'Performance')
+  t.is(title({ optionCodes: ['MDLY', 'MTY12'] }), 'Performance')
 })
