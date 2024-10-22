99fa2f6e5c6fe7317f313bbce80f0709e9f67378
Kiko Beats <josefrancisco.verdu@gmail.com>
2024-10-22 09:25:16 +0200

    chore: add MT357

--- test/model-3.js
+++ test/model-3.js
@@ -48,6 +48,7 @@ test('Model 3 Long Range AWD', t => {
   t.is(title({ optionCodes: ['MDL3', 'MT328'] }), 'Long Range AWD')
   t.is(title({ optionCodes: ['MDL3', 'MT352'] }), 'Long Range AWD')
   t.is(title({ optionCodes: ['MDL3', 'MT354'] }), 'Long Range AWD')
+  t.is(title({ optionCodes: ['MDL3', 'MT357'] }), 'Long Range AWD')
 })
 
 test('Model 3 Performance', t => {
